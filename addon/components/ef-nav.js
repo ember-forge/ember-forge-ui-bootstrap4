f342efe16718bfc2bf253e3433506c60850c6087
Jeremy Brown <jeremy@notmessenger.com>
2016-07-22 15:33:24 -0500

    Set classes on ef-list based on context

--- addon/components/ef-nav.js
+++ addon/components/ef-nav.js
@@ -21,6 +21,19 @@ export default Component.extend({
   // -------------------------------------------------------------------------
   // Actions
 
+  /**
+   * didInsertElement event hook
+   *
+   * Contextualize the list component
+   *
+   * @returns {undefined}
+   */
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.setListContext();
+  },
+
   // -------------------------------------------------------------------------
   // Events
 
@@ -32,5 +45,27 @@ export default Component.extend({
 
   // -------------------------------------------------------------------------
   // Methods
+
+  /**
+   * Set context-specific classes on these rendered components:
+   *   - ef-list
+   *   - ef-list-item
+   *
+   * @private
+   * @returns {undefined}
+   */
+  setListContext() {
+    this._super(...arguments);
+
+    // ef-list
+    this.$('.list-group')
+      .removeClass('list-group')
+      .addClass('nav');
+
+    // ef-list-item
+    this.$('.list-group-item')
+      .removeClass('list-group-item')
+      .addClass('nav-item');
+  }
 
 });
