e8d20543dea7aff6e473138f11f401e5cd585f93
Jeremy Brown <jeremy@notmessenger.com>
2016-07-22 14:11:36 -0500

    Put into developing mode

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-forge-ui-bootstrap4'
+  name: 'ember-forge-ui-bootstrap4',
+  
+  // This allows auto-reloading in host apps when they have been
+  // npm linked against this project.
+  isDevelopingAddon: function() {
+    return true;
+  }
 };
