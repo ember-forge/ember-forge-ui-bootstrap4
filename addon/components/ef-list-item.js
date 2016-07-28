e9625275091127e03aa1837b4424e95363848e12
Jeremy Brown <jeremy@notmessenger.com>
2016-07-27 19:45:20 -0500

    Remove code that is not needed since do not need to react to bound values

--- addon/components/ef-list-item.js
+++ addon/components/ef-list-item.js
@@ -50,16 +50,13 @@ export default Component.extend({
    *
    * Ordered list items do not have any classes applied
    * Unordered lists are considered list groups and items have their own class applied
-   * Unordered lists used as navs have their items class applied
+   * Unordered lists used as navs have their item's class applied
    *
    * @private
    * @returns {undefined}
    */
   setContextStyles() {
     let classNames = get(this, 'classNames');
-
-    classNames.removeObject('list-group-item');
-    classNames.removeObject('nav-item');
 
     if (!get(this, 'ordered')) {
       classNames.addObject((get(this, 'usedAs') === 'nav') ? 'nav-item' : 'list-group-item');
