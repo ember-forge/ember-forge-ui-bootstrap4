6397ed7ed77b92c5a71d3a12b94204b4439446c5
Jeremy Brown <jeremy@notmessenger.com>
2016-08-03 07:33:09 -0500

    ef-form component

---
+++ addon/components/ef-form.js
@@ -0,0 +1,48 @@
+import Component from 'ember-forge-ui/components/ef-form';
+import Ember from 'ember';
+
+const {
+  get
+} = Ember;
+
+/**
+ * @module
+ * @augments ember-forge-ui/components/ef-form
+ */
+export default Component.extend({
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  /** @type {String[]} */
+  classNameBindings: [
+    'inline:form-inline'
+  ],
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Events
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  /**
+   * Display a series of labels, form controls, and buttons on a single horizontal row.
+   *
+   * @see {@link http://v4-alpha.getbootstrap.com/components/forms/#inline-forms}
+   * @type {Boolean}
+   */
+  inline: false
+
+  // -------------------------------------------------------------------------
+  // Observers
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+});
