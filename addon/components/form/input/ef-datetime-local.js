327a5773e372ec441fe01ff7712f7570b6a426b0
Jeremy Brown <jeremy@notmessenger.com>
2016-08-15 07:16:40 -0500

    Add form/input/ef-datetime-local component

---
+++ addon/components/form/input/ef-datetime-local.js
@@ -0,0 +1,37 @@
+import Component from 'ember-forge-ui/components/form/input/ef-datetime-local';
+
+/**
+ * input[type="datetime-local"] HTML element
+ *
+ * @module
+ * @augments ember-forge-ui/components/form/input/ef-datetime-local
+ */
+export default Component.extend({
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  /** @type {String[]} */
+  classNames: [
+    'form-control'
+  ]
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Events
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  // -------------------------------------------------------------------------
+  // Observers
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+});
