bc2a0dc52da580c01b5947c126c9667a4373d537
Jeremy Brown <jeremy@notmessenger.com>
2016-09-03 12:30:02 -0500

    Add form/ef-element-error component

---
+++ addon/components/form/ef-element-error.js
@@ -0,0 +1,37 @@
+import Component from 'ember-forge-ui/components/form/ef-element-error';
+
+/**
+ * paragraph HTML element used to display informational text about a form element
+ *
+ * @module
+ * @augments ember-forge-ui/components/form/ef-element-error
+ */
+export default Component.extend({
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  /** @type {String[]} */
+  classNames: [
+    'form-control-feedback'
+  ]
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Events
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  // -------------------------------------------------------------------------
+  // Observers
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+});
