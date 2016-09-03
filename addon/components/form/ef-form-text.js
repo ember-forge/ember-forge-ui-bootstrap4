5b635c3f9423a5236ce12947526a3d082adeae68
Jeremy Brown <jeremy@notmessenger.com>
2016-09-02 19:51:07 -0500

    Add form/ef-form-text component

---
+++ addon/components/form/ef-form-text.js
@@ -0,0 +1,37 @@
+import Component from 'ember-forge-ui/components/form/ef-form-text';
+
+/**
+ * paragraph HTML element used to display informational text about a form element
+ *
+ * @module
+ * @augments ember-forge-ui/components/form/ef-form-text
+ */
+export default Component.extend({
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  /** @type {String[]} */
+  classNames: [
+    'form-text'
+  ]
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Events
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  // -------------------------------------------------------------------------
+  // Observers
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+});
