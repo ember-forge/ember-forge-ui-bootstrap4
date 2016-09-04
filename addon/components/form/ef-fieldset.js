1a741b667a763464eb2aa2d31d319305df4f7d87
Jeremy Brown <jeremy@notmessenger.com>
2016-09-03 19:01:01 -0500

    Add error state management to .form-group (form/ef-fieldset)

--- addon/components/form/ef-fieldset.js
+++ addon/components/form/ef-fieldset.js
@@ -1,18 +1,25 @@
 import Component from 'ember-forge-ui/components/form/ef-fieldset';
+import ErrorState from 'ember-forge-ui/mixins/form/error-state';
 
 /**
  * fieldset HTML element
  *
  * @module
  * @augments ember-forge-ui/components/form/ef-fieldset
+ * @augments ember-forge-ui/mixins/form/error-state
  */
-export default Component.extend({
+export default Component.extend(ErrorState, {
 
   // -------------------------------------------------------------------------
   // Dependencies
 
   // -------------------------------------------------------------------------
   // Attributes
+
+  /** @type {String[]} */
+  classNameBindings: [
+    'errorState:has-danger'
+  ],
 
   /** @type {String[]} */
   classNames: [
