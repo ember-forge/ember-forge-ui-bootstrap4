f23e57eb393ecf68a15869d384346a6b4a48c558
Jeremy Brown <jeremy@notmessenger.com>
2016-09-14 09:39:22 -0500

    Closes ember-forge/ember-forge-ui-bootstrap4#33

--- addon/components/form/ef-fieldset.js
+++ addon/components/form/ef-fieldset.js
@@ -1,12 +1,10 @@
 import Component from 'ember-forge-ui/components/form/ef-fieldset';
-import ErrorState from 'ember-forge-ui/mixins/form/error-state';
 
 /**
  * fieldset HTML element
  *
  * @module
  * @augments ember-forge-ui/components/form/ef-fieldset
- * @augments ember-forge-ui/mixins/form/error-state
  */
 export default Component.extend(ErrorState, {
 
